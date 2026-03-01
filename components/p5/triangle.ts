8f70c2213aeda15fef973d7197ed12480456eeef
plan9better <plan9better@proton.me>
2026-03-01 22:53:56 +0100

    feat(p5-triangle): sine wave vertex drawing

--- components/p5/triangle.ts
+++ components/p5/triangle.ts
@@ -16,7 +16,9 @@ export const init = () =>
 
       return (p, { width, height }, t) => {
         const s = Math.sin(t)
-        const color = s >= 0 ? 255 : 0
+        const stage = (s + 1) / 2;
+        const bgCol = stage * 255;
+        const color = s >= 0 ? p.color(255, 255, 255 - bgCol) : 0
 
         const resized = width !== lastWidth || height !== lastHeight
         if (resized) {
@@ -32,33 +34,8 @@ export const init = () =>
         const M = { x: width, y: height }
 
         p.loadPixels()
-        const historyWidth = 100
-        const futureWidth = 0
-        const waveHeight = 100
-        const totalWidth = historyWidth + futureWidth
-        const midline = Math.round(waveHeight / 2)
-        const step = 0.02
 
-        for (let x = 0; x <= totalWidth; x += 1) {
-          for (let y = 0; y <= waveHeight; y += 1) {
-            p.set(x, y, 0)
-          }
-        }
-
-        for (let x = 0; x <= totalWidth; x += 1) {
-          p.set(x, midline, p.color(0, 255, 0))
-        }
-        // for (let y = 0; y <= waveHeight; y += 1) {
-        //   p.set(historyWidth, y, p.color(0, 255, 0))
-        // }
-
-        for (let offset = -historyWidth; offset <= futureWidth; offset += 1) {
-          const screenX = offset + historyWidth
-          const y = -Math.round(Math.sin(t + offset * step) * 50) + midline
-          p.set(screenX, y, p.color(255, 0, 0))
-        }
-
-        for (let i = 0; i < 1200; i += 1) {
+        for (let i = 0; i < (Math.abs(s) * 100) * 50; i += 1) {
           const variant = Math.floor(p.random(0, 3))
           if (variant === 0) {
             cx = (cx + L.x) / 2
@@ -73,7 +50,34 @@ export const init = () =>
 
           p.set(Math.floor(cx), Math.floor(cy), p.color(color))
         }
+        const historyWidth = 100
+        const futureWidth = 0
+        const waveHeight = 100
+        const totalWidth = historyWidth + futureWidth
+        const midline = Math.round(waveHeight / 2)
+        const step = 0.02
+        for (let x = 0; x <= totalWidth; x += 1) {
+          for (let y = 0; y <= waveHeight; y += 1) {
+            p.set(x, y, 0)
+          }
+        }
         p.updatePixels()
+
+
+
+        p.stroke(0, 255, 0)
+        p.line(0, midline, totalWidth, midline)
+
+        p.stroke(255, 0, 0)
+        p.noFill();
+        p.beginShape();
+        for (let offset = -historyWidth; offset <= futureWidth; offset += 1) {
+          const screenX = offset + historyWidth
+          const y = -Math.round(Math.sin(t + offset * step) * 50) + midline
+          p.vertex(screenX, y);
+        }
+        p.endShape();
+
       }
     })(),
   })
